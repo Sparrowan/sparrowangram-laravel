cd4fee277c8f8feecb1b0efd0ca6ff3355e924bc
Alphius <alphiuswambua@gmail.com>
2020-04-20 18:41:29 +0300

    Basic setup and user setup upto 58:15

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row ">
+      <div class="col-3 p-5">
+      <img src="https://scontent-mba1-1.cdninstagram.com/v/t51.2885-19/s150x150/15535314_905147332954241_7281674707391217664_a.jpg?_nc_ht=scontent-mba1-1.cdninstagram.com&_nc_ohc=CO1sTJ9LpSkAX8oI1IO&oh=47a44e1f1bb083773853829036a9aeea&oe=5EC85919" alt="" class="rounded-circle">
+      </div>  
+      <div class="col-9">
+      <div><h1>_alphius</h1></div>
+      <div class="d-flex">
+      <div class="pr-5"><strong>66</strong> posts</div>
+      <div class="pr-5"><strong>10.3k</strong> followers</div>
+      <div class="pr-5"><strong>7487</strong> following</div>
+      </div>
+      <div class="pt-4 font-weight-bold">jack alphius sparrowan</div>
+      <div>Data scientist , Software engineer, Artificial intelligence. Full stack web and mobile developer. Python,React,R,flutter,java,PHP. CEO SPARROWAN SLNS.</div>
+      <div><a href="https://l.instagram.com/?u=https%3A%2F%2Fm.youtube.com%2Fwatch%3Ffeature%3Dyoutube_gdata_player%26v%3DovJaDP9jVoc&e=ATOsKfWMZM6zUuvVt2tgtoE8_wfTkqd7Rb24cvD0jnzXRXa28MLidAwuNrdX56PS_ZnGKLPuClwNkETM&s=1">m.youtube.com/watch?feature=youtube_gdata_player&v=ovJaDP9jVoc</a></div>
+      </div>
+    </div>
+    <div class="row">
+    <div class="col-4">
+    <img src="https://scontent-mba1-1.cdninstagram.com/v/t51.2885-15/e35/c0.108.864.864a/s150x150/67294241_101269571212891_8347627971588120860_n.jpg?_nc_ht=scontent-mba1-1.cdninstagram.com&_nc_cat=107&_nc_ohc=So1wT_EqfrwAX8rLhql&oh=102e014f938d0a822fa7a1c82acb6fb4&oe=5EC67D4A" class="w-100">
+    </div>
+    <div class="col-4">
+    <img src="https://scontent-mba1-1.cdninstagram.com/v/t51.2885-15/e35/c0.135.1080.1080a/s150x150/45535019_2170826009837775_1778829040356739161_n.jpg?_nc_ht=scontent-mba1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=eOH_pvpBTbQAX8trG04&oh=3594a56df8481eeb2f2a9b07eee391fc&oe=5EC7DE08" class="w-100">
+    </div>
+    <div class="col-4">
+    <img src="https://scontent-mba1-1.cdninstagram.com/v/t51.2885-15/e35/c180.0.720.720a/s320x320/43913201_996296030542502_7980341152304914267_n.jpg?_nc_ht=scontent-mba1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=FiQuMdSuKs8AX9kCRqx&oh=b62446bb6a566fc8ecbee7173fdf9350&oe=5EC5E17E" class="w-100">
+    </div>
+    </div>
+</div>
+@endsection
